aaa8087e9a8b56a405d4ca63d6ebb73b504c398b
Emil Linder <emil.linder1@learnet.se>
2018-10-15 09:27:53 +0200

    förbättringar i vårat spel v2

--- js_grunder/gissa_tal.js
+++ js_grunder/gissa_tal.js
@@ -2,11 +2,18 @@
 var slumptal = Math.ceil(Math.random() * 100);
 console.log('Nytt slumptal: ' + slumptal);
 
+/* antal försök */
+var antal = 0;
+
 /*våra globala variabler  */
 var elementGissning = document.querySelector('#gissning');
 var elementSvar = document.querySelector('#svar');
 /* reagera på när man trycker på knappen */
 function gissa() {
+
+    /* räkna upp */
+    antal++;
+
     /* läsa av gissningen i första input-rutan */
         var gissning = elementGissning.value;
         console.log('Gissningen: '+gissning);
@@ -14,21 +21,25 @@ function gissa() {
     /* om gissning = talet, då har man vunnit */
     if (gissning ==slumptal) {
         /* skriv i andra input-rutan */
-        elementSvar.value = 'Yipppee! Du vann!'
+        svar = ' <p class="animated yipee">' + antal + ')' + gissning + 'Yippee! Du vann!</p > ';
+        elementSvar.insertAdjacentHTML('beforeend', svar);
     }
 
     /* om gissning > talet, skriv ut "för högt" */
     if (gissning > slumptal) {
-        elementSvar.value = 'För högt';
+        svar = '<p class="hogt">' + antal + ')' + gissning + 'för högt!</p>';
+        elementSvar.insertAdjacentHTML('beforeend', svar);
     }
 
     /* om gissning < talet , skriv ut "för lågt" */
-    if (gissning < slumptal) {
-        elementSvar.value = 'För lågt';
+    if (gissning < slumptal){
+        svar = '<p class="lagt">' + antal + ')' + gissning + 'för lågt!</p>';
+        elementSvar.insertAdjacentHTML('beforeend', svar);
     }
 
     /* om gissning > 100, skriv ut "är du dum" */
     if (gissning > 100) {
-        elementSvar.value = 'Är du dum!';
+        svar = '<p class="dum">' + antal + ')' + gissning + 'är du dum!</p>';
+        elementSvar.insertAdjacentHTML('beforeend', svar);
     }
 }
