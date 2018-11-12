62e47479f774bb1d6fde43f3ec99224d4d4e7f7d
Emil Linder <emil.linder1@learnet.se>
2018-11-12 09:42:07 +0100

    övet på slider range

---
+++ js_grunder/range.labb/range.js
@@ -0,0 +1,7 @@
+const elementSlider = document.querySelector('.slidekontainer')
+
+elementSlider.addEventListener('slide', andraStorlek)
+
+function andraStorlek() {
+    console.log(andraStorlek+1)
+}
