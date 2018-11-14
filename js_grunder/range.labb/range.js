b565552997d1fe0b4dec818280f73d77035d4c86
Emil Linder <emil.linder1@learnet.se>
2018-11-14 14:56:27 +0100

    tränat på HTML slider, querySelector, event (input) och stränhantering.

--- js_grunder/range.labb/range.js
+++ js_grunder/range.labb/range.js
@@ -1,7 +1,32 @@
-const elementSlider = document.querySelector('.slidekontainer')
+window.onload = start;
 
-elementSlider.addEventListener('slide', andraStorlek)
+function start() {
+    /* elementen vi jobbar med */
+    const elementImg = document.querySelector('img');
+    const elementBredd = document.querySelector('#bredd');
+    const elementHojd = document.querySelector('#hojd');
+    const elementOutput = document.querySelector('#output')
 
-function andraStorlek() {
-    console.log(andraStorlek+1)
+    elementImg.width = 500;
+    elementImg.height = 400;
+
+    /* lyssna på när man drar i slidern */
+    elementBredd.addEventListener('input', andraBredd);
+
+    elementHojd.addEventListener('input', andraHojd);
+
+    function andraBredd() {
+        console.log('Bredd=' + elementBredd.value);
+        elementImg.width = elementBredd.value;
+        elementOutput.textContent = '(' + elementBredd.value + 'x' + elementHojd.value + ')';
+    }
+
+    function andraHojd() {
+        console.log('höjd=' + elementHojd.value);
+        elementImg.height = elementHojd.value;
+        elementOutput.textContent = '(' + elementBredd.value + 'x' + elementHojd.value + ')';
+    }
+
 }
+
+
